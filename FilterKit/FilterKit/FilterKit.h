4bdfba763c468c0efc4ee9bee180479288a08d68
Matt Jarjoura <matt@artcator.com>
2012-07-21 10:04:26 -0700

    Adding FilterKit framework

---
+++ FilterKit/FilterKit/FilterKit.h
@@ -0,0 +1,13 @@
+//
+//  FilterKit.h
+//  FilterKit
+//
+//  Created by Matt Jarjoura on 7/21/12.
+//  Copyright (c) 2012 iOS Dev Camp 2012. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FilterKit : NSObject
+
+@end
