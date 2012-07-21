b42fb9cd14a326d191b1bc3a76420460315f9ddd
Scott Tran <scott.tran@gmail.com>
2012-07-21 15:22:56 -0700

    added overlay filter and test images

---
+++ FilterKit/FilterKit/FKOverlayFilter.h
@@ -0,0 +1,17 @@
+//
+//  Created by stran on 7/21/12.
+//
+//
+
+
+#import <Foundation/Foundation.h>
+#import "FKFilter.h"
+
+
+@interface FKOverlayFilter : NSObject <FKFilter> {
+
+
+}
+@property(nonatomic, strong) UIImage *overlayImage;
+
+@end
