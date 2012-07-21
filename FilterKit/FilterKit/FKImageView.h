afbae62d5947c4be0b274bde43be49fdea83312d
Matt Jarjoura <matt@artcator.com>
2012-07-21 12:09:31 -0700

    Adding some starting point classes

---
+++ FilterKit/FilterKit/FKImageView.h
@@ -0,0 +1,13 @@
+//
+//  FKImageView.h
+//  FilterKit
+//
+//  Created by Matt Jarjoura on 7/21/12.
+//  Copyright (c) 2012 iOS Dev Camp 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FKImageView : UIView
+
+@end
---
+++ FilterKit/FilterKit/FKImageView.m
@@ -0,0 +1,31 @@
+//
+//  FKImageView.m
+//  FilterKit
+//
+//  Created by Matt Jarjoura on 7/21/12.
+//  Copyright (c) 2012 iOS Dev Camp 2012. All rights reserved.
+//
+
+#import "FKImageView.h"
+
+@implementation FKImageView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
