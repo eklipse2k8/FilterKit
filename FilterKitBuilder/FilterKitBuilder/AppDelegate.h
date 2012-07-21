bc919d04be3de4156331e272ab2ca59fe08f9ad2
Scott Tran <scott.tran@gmail.com>
2012-07-21 16:37:48 -0700

    added filterkitbuilder project

---
+++ FilterKitBuilder/FilterKitBuilder/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FilterKitBuilder
+//
+//  Created by Scott Tran on 7/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
