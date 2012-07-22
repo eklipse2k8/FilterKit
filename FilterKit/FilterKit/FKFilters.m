eded627b4a38b88300d97f152e04e1e6cf06496b
Matt Jarjoura <matt@artcator.com>
2012-07-21 20:43:08 -0700

    Adding a little description of it

--- FilterKit/FilterKit/FKFilters.m
+++ FilterKit/FilterKit/FKFilters.m
@@ -8,6 +8,13 @@
 
 #import "FKFilters.h"
 
+// Below is a list of classes in order of how you want them available in your app.
+// These are just strings that will later be called by NSClassFromString when FKImageView is
+// about to call processFilterChain
+
+// If you plan to disable a filter, make sure to uncheck it from the framework so that it
+// doesn't get compiled into the application.
+
 const NSString * const FKFilterList[] = {
     @"FKBlackWhiteFilter",
     @"FKLightLeakFilter",
