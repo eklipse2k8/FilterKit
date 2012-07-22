1eef43629cfbb5d596f5cdfd60ed6ccfed726bce
Matt Jarjoura <matt@artcator.com>
2012-07-21 20:39:56 -0700

    FKFilters is a list of available filters

---
+++ FilterKit/FilterKit/FKFilters.h
@@ -0,0 +1,11 @@
+//
+//  FKFilters.h
+//  FilterKit
+//
+//  Created by Matt Jarjoura on 7/21/12.
+//  Copyright (c) 2012 iOS Dev Camp 2012. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+extern const NSString * const FKFilterList[];
---
+++ FilterKit/FilterKit/FKFilters.m
@@ -0,0 +1,16 @@
+//
+//  FKFilters.m
+//  FilterKit
+//
+//  Created by Matt Jarjoura on 7/21/12.
+//  Copyright (c) 2012 iOS Dev Camp 2012. All rights reserved.
+//
+
+#import "FKFilters.h"
+
+const NSString * const FKFilterList[] = {
+    @"FKBlackWhiteFilter",
+    @"FKLightLeakFilter",
+    nil
+};
+
